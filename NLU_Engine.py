0af67685cf47053c47181718cf3cc95eeeeeea97
ZiadMaged <Ziad_7@live.com>
2019-06-08 11:49:08 +0200

    Adding Project Requirements

--- NLU_Engine.py
+++ NLU_Engine.py
@@ -6,7 +6,6 @@ import snips_nlu_en
 from snips_nlu import SnipsNLUEngine, load_resources
 from snips_nlu.default_configs import CONFIG_EN
 from googletrans import Translator
-import app
 from DBConnection import Database
 from DB_Interaction import interaction
 
@@ -129,7 +128,7 @@ class NLU:
         if self.checkIntent():
             if str(self.CheckJsonEntities()).__contains__("Has No Entities"):
                 print("Chatbot : Get Answer From Json Answers")
-                app.send_message(self.User_id,"Get Answer From Json Answers")
+                app.send_message(self.User_id,'not available due to intent')
             else:
                 self.return_original()
         else:
@@ -257,3 +256,97 @@ class NLU:
                 print("-----------")
          print("Exit with No Entities")
          return "Has No Entities"
+
+
+#Python libraries that we need to import for our bot
+import random
+from flask import Flask, request
+from pymessenger.bot import Bot
+
+app = Flask(__name__)
+ACCESS_TOKEN = 'EAAGGGCVi2rcBALuP2tFhFnyWbu10RF7CHWpFwfvfyeZC1hfzpR1N9ZAShQqgeqDPZCGFk1o3POMCnDtVXaByFJlV46K1TT1saiuGel70cpeqPTbpIF033DQgSPoozDA9zjZARZAJ9CbUfXrXFeHIKZCmroNKfAtFDCcKZBmcGh0UwZDZD'
+VERIFY_TOKEN = 'VERIFY_TOKEN'
+bot = Bot(ACCESS_TOKEN)
+
+#We will receive messages that Facebook sends our bot at this endpoint
+@app.route("/", methods=['GET', 'POST'])
+def receive_message():
+    if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook."""
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    else:
+        # get whatever message a user sent the bot
+       output = request.get_json()
+       for event in output['entry']:
+          messaging = event['messaging']
+          for message in messaging:
+            if message.get('message'):
+                #Facebook Messenger ID for user so we know where to send response back to
+                recipient_id = message['sender']['id']
+                if message['message'].get('text'):
+                    response_sent_text = message['message'].get('text')
+                    send_message(recipient_id, response_sent_text)
+                #if user sends us a GIF, photo,video, or any other non-text item
+                if message['message'].get('attachments'):
+                    response_sent_nontext = message['message'].get('text')
+                    send_message(recipient_id, response_sent_nontext)
+    return "Message Processed"
+
+
+def verify_fb_token(token_sent):
+    #take token sent by facebook and verify it matches the verify token you sent
+    #if they match, allow the request, else return an error
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+
+
+#chooses a random message to send to the user
+def get_message():
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
+
+def GetMessageFromUser():
+     if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook."""
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+     else:
+        # get whatever message a user sent the bot
+       output = request.get_json()
+       for event in output['entry']:
+          messaging = event['messaging']
+          for message in messaging:
+            if message.get('message'):
+                #Facebook Messenger ID for user so we know where to send response back to
+                recipient_id = message['sender']['id']
+                if message['message'].get('text'):
+                   return message['message'].get('text') , recipient_id
+                    # send_message(recipient_id, response_sent_text)
+                #if user sends us a GIF, photo,video, or any other non-text item
+                # if message['message'].get('attachments'):
+                #     response_sent_nontext = message['message'].get('text')
+                #     send_message(recipient_id, response_sent_nontext)
+     # return "Message Processed"
+
+
+#uses PyMessenger to send response to user
+def send_message(recipient_id, response):
+    #sends user the text message provided via input response parameter
+    bot.send_text_message(recipient_id, response)
+    return "success"
+
+if __name__ == "__main__":
+    app.run()
+    nlu = NLU()
+    nlu.EngineMode("Train")
+        # nlu.setQuery(Translate_Text(question))
+    nlu.setQuery(GetMessageFromUser())
+
+    nlu.answer()
